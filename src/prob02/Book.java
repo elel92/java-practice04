afc1b9f0e0e03ff52b88ae55b279bfd0f0ba100f
BIT <BIT@DESKTOP-Q04GE57>
2019-08-09 17:08:32 +0900

    2번 완료

--- src/prob02/Book.java
+++ src/prob02/Book.java
@@ -4,9 +4,17 @@ public class Book {
 	private int bookNo;
 	private String title;
 	private String author;
-	private int stateCode;
+	private int stateCode = 1;
 	
-
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+	}
+	
+	public int getBookNo() {
+		return bookNo;
+	}
 	
 	public void print() {
 		System.out.println(
@@ -14,4 +22,11 @@ public class Book {
 			", 작가:" + author +
 			", 대여 유무:" + ( stateCode == 1 ? "재고있음" : "대여중" ) );
 	}
+
+	public void rent() {
+		stateCode = 0;
+		System.out.println(title + "이(가) 대여 됐습니다.");
+	}
+
+	
 }
